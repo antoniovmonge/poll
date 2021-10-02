736325198db833d004a1488a5034c11a83b290d9
Antonio Vilchez <antoniovmonge@gmail.com>
2021-10-02 18:35:38 +0200

    reviewing code hidding DATABASE-URI

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
 import psycopg2
-from psycopg2.errors import DivisionByZero
+# from psycopg2.errors import DivisionByZero (only necesary for pycharm)
 from dotenv import load_dotenv
 import database
 
 
 DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
-MENU_PROMPT = """-- Menu --
+MENU_PROMPT = """
+-- Menu --
 
 1) Create new poll
 2) List open polls
@@ -15,7 +16,8 @@ MENU_PROMPT = """-- Menu --
 5) Select a random winner from a poll option
 6) Exit
 
-Enter your choice: """
+Enter your choice:
+"""
 NEW_OPTION_PROMPT = "Enter new option text (or leave empty to stop adding options): "
 
 
